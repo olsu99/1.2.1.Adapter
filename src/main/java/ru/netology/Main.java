a3081c1a9311e33b981f1f49de7e146cb3b31774
olsu99 <alsou-061999@mail.ru>
2023-03-29 23:26:55 +0300

    initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.pow(2, 0));
+        System.out.println(intsCalc.mult(1, 3));
+    }
+}
